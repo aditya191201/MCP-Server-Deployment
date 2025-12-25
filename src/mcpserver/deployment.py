138ad28bdda894762ecf1ce0cfb84d7717048f69
Aditya Deshpande <71213262+aditya191201@users.noreply.github.com>
2025-12-25 11:41:18 -0500

    Created a basic mcp server to deploy

---
+++ src/mcpserver/deployment.py
@@ -0,0 +1,11 @@
+#server.py
+
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("Demo")
+
+# Add an addition tool
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
